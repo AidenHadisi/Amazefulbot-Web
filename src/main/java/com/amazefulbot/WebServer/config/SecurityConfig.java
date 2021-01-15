1ddceacf36d05410ff02d704b480b2112b198478
Aiden H <so.hadisi@gmail.com>
2021-01-14 19:07:22 -0800

    Allow usage of security role and pre/post annotations.

--- src/main/java/com/amazefulbot/WebServer/config/SecurityConfig.java
+++ src/main/java/com/amazefulbot/WebServer/config/SecurityConfig.java
@@ -7,6 +7,7 @@ package com.amazefulbot.WebServer.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private  TwitchUserService oAuth2UserService;
